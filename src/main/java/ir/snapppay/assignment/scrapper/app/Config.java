3bd0b1933b3975634e64f417f86fa3d4b05943b9
mehmaj <mehran.majidi@gmail.com>
2023-09-20 23:49:12 +0330

    Add swagger Add config.properties Add lombok

---
+++ src/main/java/ir/snapppay/assignment/scrapper/app/Config.java
@@ -0,0 +1,9 @@
+package ir.snapppay.assignment.scrapper.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:config.properties")
+public class Config {
+}
